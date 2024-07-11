9b29a9a22804d454596a438cce8d067c41a4e4eb
Artu30 <gonzalez.arturo1991@gmail.com>
2024-07-11 09:09:00 +0200

    Accessing our Subsystem

---
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Public/Menu.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Menu.generated.h"
+
+class UMultiplayerSessionsSubsystem;
+class UButton;
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSESSIONS_API UMenu : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void MenuSetup();
+
+protected:
+	virtual bool Initialize() override;
+	
+private:
+	UPROPERTY(meta=(BindWidget))
+	UButton* HostButton;
+
+	UPROPERTY(meta=(BindWidget))
+	UButton* JoinButton;
+
+	UFUNCTION()
+	void HostButtonClicked();
+
+	UFUNCTION()
+	void JoinButtonClicked();
+
+	// The subsystem designed to handle all online session functionality
+	UPROPERTY()
+	UMultiplayerSessionsSubsystem* MultiplayerSessionsSubsystem;
+	
+};
