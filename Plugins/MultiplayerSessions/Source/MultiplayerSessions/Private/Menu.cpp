712758c250b46cb547b339df5751cdfadc1aa254
Artu30 <gonzalez.arturo1991@gmail.com>
2024-07-12 12:55:51 +0200

    Path to Lobby

--- Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
+++ Plugins/MultiplayerSessions/Source/MultiplayerSessions/Private/Menu.cpp
@@ -6,8 +6,9 @@
 #include "Interfaces/OnlineSessionInterface.h"
 #include "OnlineSubsystem.h"
 
-void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch)
+void UMenu::MenuSetup(int32 NumberOfPublicConnections, FString TypeOfMatch, FString LobbyPath)
 {
+	PathToLobby = FString::Printf(TEXT("%s?listen"), *LobbyPath);
 	NumPublicConnections = NumberOfPublicConnections;
 	MatchType = TypeOfMatch;
 	AddToViewport();
@@ -83,7 +84,7 @@ void UMenu::OnCreateSession(bool bWasSuccessful)
 		UWorld* World = GetWorld();
         if (World)
         {
-        	World->ServerTravel("/Game/ThirdPerson/Maps/Lobby?listen");
+        	World->ServerTravel(PathToLobby);
         }
 	}
 	else
